5e764bc3a7ce1a8c72fc00814b8000dd022c926b
M. Edi Sujarwo <edi.sujarwo@koltiva.com>
2025-01-01 01:57:42 +0700

    WIP

---
+++ src/main/groovy/com/exa/spring_boot_crud_operations/SpringBootCrudOperationsApplication.groovy
@@ -0,0 +1,13 @@
+package com.exa.spring_boot_crud_operations
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringBootCrudOperationsApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudOperationsApplication, args)
+	}
+
+}
